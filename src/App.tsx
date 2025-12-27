8ad1b03d9caf86c5183caba6a7181e84999d5464
MartellHub <lesnik669@gmail.com>
2025-12-27 11:43:44 +0200

    nothing happend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import './App.css'
-
-import Home from './appComponents/Home'
-import Header from './appComponents/Header'
-import Footer from './appComponents/Footer'
-import Chat from './chatComponents/Chat'
-import About from './appComponents/About'
-
+import Header from './appComponents/Header';
+import Home from './appComponents/Home';
+import About from './appComponents/About';
+import Footer from './appComponents/Footer';
+import './App.css';
 
 function App() {
   return (
-    <div className='flex flex-col w-screen h-screen'>
-      <Header />
-      <main className="flex-1 overflow-y-auto">
-      <div className="min-h-full flex flex-col items-center">
-        <Home />
-        <About />
+    <div>
+      <div className='flex flex-col w-screen h-screen'>
+        <Header />
+        <main className='flex-1 overflow-y-auto'>
+          <div className='min-h-full flex flex-col items-center'>
+            <Home />
+            <About />
+          </div>
+        </main>
+        <Footer />
       </div>
-    </main>
-      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
