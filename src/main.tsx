e6f9c838accde3298230d4227a328c03ee2f8066
MartellHub <lesnik669@gmail.com>
2025-12-30 18:51:59 +0200

    added something

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider  , createBrowserRouter} from 'react-router-dom'
 import { AuthProvider } from '../firebase/AuthContext';
-import App from './App.tsx';
+
+import App from './App';
+import Chat from './chatComponents/Chat';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/Chat',
+    element: <Chat />
+  }
+
+
+]);
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router = {router}/> 
   </AuthProvider>,
 )
+    {/* <RouterProvider router = {router}/> */}
